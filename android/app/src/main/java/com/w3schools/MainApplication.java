f2ac9f8ca0c86c39f83cdde6121bc205965f75db
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-05-19 17:57:10 +0530

    file structure

--- android/app/src/main/java/com/w3schools/MainApplication.java
+++ android/app/src/main/java/com/w3schools/MainApplication.java
@@ -3,6 +3,7 @@ package com.w3schools;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -21,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
+      return Arrays.<ReactPackage>asList(new MainReactPackage(), new RNCWebViewPackage());
     }
 
     @Override
